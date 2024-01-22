e9cb56b53ef4286a3a7f1b61f761233b96fd3df1
Swastik <swastikpanda32@gmail.com>
2024-01-22 20:09:40 +0530

    splitted components and added edit functionality to players

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player1" symbol="X" />
+          <Player name="Player2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
-
-export default App
+export default App;
